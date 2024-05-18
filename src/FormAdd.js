c48426e2539edca5e61b886295b7c22e405c01c5
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-05-19 00:35:51 +0100

    feat: Add name  state to FormAddFriend component

--- src/FormAdd.js
+++ src/FormAdd.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import Button from "./Button";
 const FormAddFriend = () => {
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("");
   return (
     <form className="form-add-friend">
+    
     <label>👫 Friend name</label>
-    <input type="text" />
+    <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
 
     <label>🌄 Image URL</label>
     <input type="text" />
