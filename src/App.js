f3b3d2a9ba04170a1df9911906e32e270d7151f7
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-05-19 00:11:29 +0100

    feat: Add lazy loading for images in Friend component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import { useState } from "react";
 import FormAddFriend from "./FormAdd";
 import { FriendsList } from "./FriendsList";
 import Button from "./Button";
 import { FormSplitBill } from "./FormSplitBill";
 
 const App = () => {
+  const [showAddFriend, setShowAddFriend] = useState(false);
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriend />
+       {showAddFriend && <FormAddFriend />}
         <Button>Add friend</Button>
       </div>
       <FormSplitBill />
