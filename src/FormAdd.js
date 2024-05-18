5df7b728bf1e3c62b2636fc4d7482e5832b3f6f4
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-05-19 00:39:07 +0100

    feat: add image state to FormAdd.js components

--- src/FormAdd.js
+++ src/FormAdd.js
@@ -10,7 +10,7 @@ const FormAddFriend = () => {
     <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
 
     <label>🌄 Image URL</label>
-    <input type="text" />
+    <input type="text" value={image} onChange={(e) => setImage(e.target.value)} />
 
     <Button>Add</Button>
     </form>
