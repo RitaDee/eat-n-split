b21315fd2c13cf5cfd1ab55106204f9616dbd5a0
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-05-20 18:28:20 +0100

    feat: Update default image URL in FormAdd.js component

--- src/FormAdd.js
+++ src/FormAdd.js
@@ -1,10 +1,32 @@
 import { useState } from "react";
 import Button from "./Button";
+
 const FormAddFriend = () => {
     const [name, setName] = useState("");
-    const [image, setImage] = useState("");
+    const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+    const handleSubmit = ((e) => {
+        e.preventDefault();
+
+        const id = crypto.randomUUID()
+        
+        if(!name || !image) return;
+
+        const newFriend = {
+            id,
+            name,
+            image: image || "https://i.pravatar.cc/48",
+            balance: 0,
+        };
+
+        console.log(newFriend);
+
+        setName("");
+        setImage("https://i.pravatar.cc/48");
+
+    })
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
     
     <label>👫 Friend name</label>
     <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
