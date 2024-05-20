3a8f5967995ccda8febe637683f88f9dbcaae2d8
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2024-05-20 22:34:04 +0100

    feat: Update FriendsList component to accept friends prop

--- src/App.js
+++ src/App.js
@@ -4,7 +4,29 @@ import { FriendsList } from "./FriendsList";
 import Button from "./Button";
 import { FormSplitBill } from "./FormSplitBill";
 
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
 const App = () => {
+  const [friends, setFriends] = useState(initialFriends);
   const [showAddFriend, setShowAddFriend] = useState(false);
 
   const handleShowAddFriend = () => {
@@ -13,7 +35,7 @@ const App = () => {
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList />
+        <FriendsList friends={friends} />
        {showAddFriend && <FormAddFriend />}
         <Button onClick={handleShowAddFriend}>{showAddFriend ? "Close" : "Add friend"}</Button>
       </div>
